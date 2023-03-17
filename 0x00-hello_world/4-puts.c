43c7095cac347a2edbed2428a7d9ca5b509c8d5d
adam kamal metwalt <adaam.kammal@gmail.com>
2023-03-16 17:47:48 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 /**
- * void main(void)
- * {
- * int 0=0;
- * puts("\"Programming is like building a multilingual puzzle\n");
- * puts("\"%d",0);
- * } 
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
