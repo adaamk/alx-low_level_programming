bc33058eb705ded2cb9cbe24d9a8928d697c3999
adam kamal metwalt <adaam.kammal@gmail.com>
2023-03-18 18:27:08 -0700

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - lower case alphabets.c
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+{
+	putchar(ch);
+}
+putchar("\n');
+return (0);
