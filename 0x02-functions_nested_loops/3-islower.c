199aa126e99f3d6e52b7686739c1f2aac56b6694
adam kamal metwalt <adaam.kammal@gmail.com>
2023-03-23 10:38:11 -0700

    elhamdullah

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _islower - Write a function that checks for lowercase character
+ * @c: is the char to be checked
  * Return: 1 if char is lowercase, otherwise 0.
  */
 int _islower(int c)
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _isalpha - function that checks for alphabetic character
+ * @c: the character to be checked
  * Return: 1 if char is a letter,0 otherwise
  */
 int _isalpha(int c)
